f514577c363d0ce4c13665d52c99ceb238480ec7
Andrew Bunday <andrew.bunday@baseblack.com>
2011-04-19 10:32:54 +0100

    created new setup.py script for installation of tractor.api package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='tractor-api',
+	version='3.0',
+	description='Tractor script making api in python',
+	author='Andrew Bunday',
+	author_email='andrew.bunday@baseblack.com',
+	maintainer_email='requests@baseblack.com',
+	#py_modules=['shotgun.api.shotgun_api3'],
+	packages=['tractor']
+	)
