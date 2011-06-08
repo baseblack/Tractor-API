dbddabba21abf4bfadfb9120ecb616f390555e35
Andrew Bunday <andrew.bunday@gmail.com>
2011-06-08 12:42:41 +0100

    Starting to make progress on extending the serialization of the nodes to cover all of the features of the alfscript api.
    Signed-off-by: Andrew Bunday <andrew.bunday@gmail.com>

--- src/lib/tractor/api/serialize.py
+++ src/lib/tractor/api/serialize.py
@@ -25,86 +25,122 @@ class UnserializingError( SerialError ):
 	pass
 	
 class Serializer(  ):
+	"""Tractor based serialization class. When spool() is called on a job object the 
+	tree of tasks is walked down, serializing all nodes in the tree into their alf script
+	equivalents.
+	"""
+
+	jobSerializableAttrs  = [ 'globalvars', 'tags', 'tasks' ]
+	taskSerializableAttrs = {}
+	cmdSerializableAttrs = {}
 
 	def __init__(self, obj):
 		if isinstance(obj, TaskTree):
 			self.tree = obj
-			
+						
 		self.now =  int( time.time() )  # <-- should alos get this from the tree
 		self.spooldir = os.path.join( SPOOL_DIRECTORY, self.tree.user )	
 		self.spoolfile = "%s/%s.tkr" % (self.spooldir, self.now)
-
-	def printScript( self ):
-		"""A simple dump to stdout debug function"""
-		if hasattr( self, 'jobscript'):
-			print self.jobscript
-			return
-		else:
-			self.serialize()
-			self.printScript()
-					
+				
 	def serialize( self ):
 		if hasattr( self, 'tree' ):
 			self.jobscript = self.writeTask( self.tree)
+	
+	
+	def writeNode( self, node ):
+			
+		if isinstance( task, Job ):
+			output = self.writeJob( task )
+		elif isinstance( task, Task ):
+			output = self.writeTask( task )
+	
+	##### serialization #####
+	
+	def subtasks( self, node ):
+		output = "-subtasks {\n"
+		
+		for each in node.tasks:
+			output += self.writeNode( node.tasks[each] )
+		output += "\n}"
+	
+		return output
+	
+	def commands( self, node ):
+		return "-cmds { %s \n\t}" % self.writeCmds( task )
+		
+	def init( self, node ):
+		output + "-init { "
+		for var in node.globalvars:
+			output += "\nAssign %s {%s}" % ( var, node.globalvars[var] )
+		output += "\n}"
+			
+	def tags( self, node ):
+		return " -tags {%s}" % ",".join( node.tags )
+			
+	##### node iteration #####		
 			
 	def writeJob( self, job ):		
-		# job initialiazation section
+		
 		output = "\nJob -title { %s } " % job.title
+
 		if job.globalvars : 
-			output += "-init { "
-			for var in job.globalvars:
-				output += "\nAssign %s {%s}" % ( var, job.globalvars[var] )
-			output += "\n}" 	
+			output += self.init( job ) 	
+			
+		if job.tags:
+			output += self.tags( job )
+		
+		if job.tasks:
+			output += self.subtasks( job )
 		
 		return output
 			
 	def writeTask( self, task ):
 		
-		if isinstance( task, Job ):
-			output = self.writeJob( task )
-		else:
-			output = "\nTask { %s } -id { %s } " % ( task.label, task.name  )
+		output = "\nTask { %s } -id { %s } " % ( task.label, task.name  )
 			
 		if task.serialsubtasks :
 			output += "-serialsubtasks 1 "
 					
 		if task.tasks:
-			# iterate through the children
-			output += "-subtasks {\n"
-			for each in task.tasks:
-				output += self.writeTask( task.tasks[each] )
-			output += "\n} "
+			output += self.subtasks( task )
 		
-		if hasattr(task, 'commands'):
-		# finally serialize the current task
-		# but only if a command has been set. Tasks can be used to group child tasks.
-			if task.commands:
-				output += "-cmds { %s \n\t}" % self.writeCmd( task )
+		if task.commands:
+			output += self.commands( task )
 											
 		return output
-				
-	def writeCmd( self, task ):
+
+	def writeRemoteCmd( self, command, options ):
+		
+		if command.shell:
+			return "\n\t\tRemoteCmd { %s {%s %s} } -service {%s}" % ( command.shell, command.executable, options, command.service)
+		else:
+			return "\n\t\tRemoteCmd { %s %s } -service {%s}" % ( command.executable, options, command.service)
+
+	def writeLocalCmd( self, command, options ):
+		
+		if command.shell:
+			return "\n\t\tCmd { %s %s %s }" % ( command.shell, command.executable, options)
+		else:
+			return "\n\t\tCmd { %s %s }" % ( command.executable, options)		
+	
+	def writeCmds( self, task ):
 		#hand over the task these commands belong to as it may hold some information needed by each command
 		
-		output = ""
 		for command in task.commands:
 			options = "".join([ '%s %s ' % (key, value) for key, value in command.flags.items() ])
+			
 			if command.remote:	
-				if command.shell:
-					output += "\n\t\tRemoteCmd { %s {%s %s} } -service {%s}" % ( command.shell, command.executable, options, command.service)
-				else:
-					output += "\n\t\tRemoteCmd { %s %s } -service {%s}" % ( command.executable, options, command.service)
+				output = self.writeRemoteCmd( command, options )
 			else:
-				if command.shell:
-					output += "\n\t\tCmd { %s %s %s }" % ( command.shell, command.executable, options)
-				else:
-					output += "\n\t\tCmd { %s %s }" % ( command.executable, options)
+				output = self.writeLocalCmd( command, options )
+				
 			if command.tags:
-				output += " -tags {%s}" % ",".join( command.tags )
+				self.tags( command )
+				
 		return output
 
 		
-	def spool( self, destination, startpaused=True, user=None ):
+	def spool( self, destination=None, startpaused=True, user=None ):
 		"""The destination can be any one of 'stdout'/'tractor'/'disk'. To launch the render but not have it
 		start immediately set startpaused to True. To launch the job to run as another user set user to the 
 		username of the person to run as.
@@ -113,9 +149,6 @@ class Serializer(  ):
 		if not hasattr(self, 'jobscript'):
 			self.serialize()
 		
-		if destination is 'stdout':
-			print self.jobscript
-			
 		if destination is 'disk' or destination is 'tractor':
 			if not os.path.exists( self.spooldir ):
 				os.makedirs( self.spooldir )
@@ -138,3 +171,5 @@ class Serializer(  ):
 			except:
 				print "Error Calling tractor-spool.py. Please refer to your Jersey Cow for udder help"
 				
+		if destination is 'stdout' or not destination :
+			print self.jobscript
