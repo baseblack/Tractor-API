d2a35261036120e612b9dbde4c2192b8fdb8d8b8
Andrew Bunday <andrew.bunday@gmail.com>
2011-06-09 12:55:40 +0100

    Serializer class now inherits from a SerializerBase class. This has been done to split apart the logic of iterating through the job tree and the serialization into alfscript specific format.
    This will allow other serializers to be written more easily, such as a json one, without having
    to duplicate lots of code.

--- src/lib/tractor/api/serialize.py
+++ src/lib/tractor/api/serialize.py
@@ -24,16 +24,10 @@ class UnserializingError( SerialError ):
 	object. Could be a bunch of reasons really."""
 	pass
 	
-class Serializer(  ):
-	"""Tractor based serialization class. When spool() is called on a job object the 
-	tree of tasks is walked down, serializing all nodes in the tree into their alf script
-	equivalents.
-	"""
-
-	jobSerializableAttrs  = [ 'globalvars', 'tags', 'tasks' ]
-	taskSerializableAttrs = {}
-	cmdSerializableAttrs = {}
-
+class SerializerBase( object ):
+	"""The base class for the serializer handles the iteration of the job tree and writing of the serialized
+	tree to disk. It handles calling the submission script provided by the render manager"""
+	
 	def __init__(self, obj):
 		if isinstance(obj, TaskTree):
 			self.tree = obj
@@ -41,88 +35,46 @@ class Serializer(  ):
 		self.now =  int( time.time() )  # <-- should alos get this from the tree
 		self.spooldir = os.path.join( SPOOL_DIRECTORY, self.tree.user )	
 		self.spoolfile = "%s/%s.tkr" % (self.spooldir, self.now)
+		
+		super( SerializerBase, self).__init__()
 				
 	def serialize( self ):
 		if hasattr( self, 'tree' ):
-			self.jobscript = self.writeTask( self.tree)
-	
-	
+			self.jobscript = self.writeNode( self.tree)
+				
 	def writeNode( self, node ):
 			
-		if isinstance( task, Job ):
-			output = self.writeJob( task )
-		elif isinstance( task, Task ):
-			output = self.writeTask( task )
-	
-	##### serialization #####
-	
-	def subtasks( self, node ):
-		output = "-subtasks {\n"
-		
-		for each in node.tasks:
-			output += self.writeNode( node.tasks[each] )
-		output += "\n}"
-	
+		if isinstance( node, Job ):
+			output = self.writeJob( node )
+		elif isinstance( node, Task ):
+			output = self.writeTask( node )
+
 		return output
 	
-	def commands( self, node ):
-		return "-cmds { %s \n\t}" % self.writeCmds( task )
-		
-	def init( self, node ):
-		output + "-init { "
-		for var in node.globalvars:
-			output += "\nAssign %s {%s}" % ( var, node.globalvars[var] )
-		output += "\n}"
-			
-	def tags( self, node ):
-		return " -tags {%s}" % ",".join( node.tags )
-			
-	##### node iteration #####		
-			
 	def writeJob( self, job ):		
 		
-		output = "\nJob -title { %s } " % job.title
+		output = self.jobtitle( job ) 
 
-		if job.globalvars : 
-			output += self.init( job ) 	
-			
-		if job.tags:
-			output += self.tags( job )
-		
-		if job.tasks:
-			output += self.subtasks( job )
-		
+		if job.after		: output += self.subtasks( job )
+		if job.globalvars	: output += self.init( job ) 	
+		if job.tasks 		: output += self.subtasks( job )	
+		if job.atleast > 0	: output += self.subtasks( job )
+		if job.atmost > 0 	: output += self.subtasks( job )
+		if job.tags 		: output += self.tags( job )
+		if job.service 		: output += self.service( job )
+				
 		return output
 			
 	def writeTask( self, task ):
 		
-		output = "\nTask { %s } -id { %s } " % ( task.label, task.name  )
+		output = self.tasktitle( task )
 			
-		if task.serialsubtasks :
-			output += "-serialsubtasks 1 "
-					
-		if task.tasks:
-			output += self.subtasks( task )
-		
-		if task.commands:
-			output += self.commands( task )
+		if task.serialsubtasks : output += self.serialsubtasks( task )
+		if task.tasks : output += self.subtasks( task )
+		if task.commands : output += self.commands( task )
 											
 		return output
-
-	def writeRemoteCmd( self, command, options ):
 		
-		if command.shell:
-			return "\n\t\tRemoteCmd { %s {%s %s} } -service {%s}" % ( command.shell, command.executable, options, command.service)
-		else:
-			return "\n\t\tRemoteCmd { %s %s } -service {%s}" % ( command.executable, options, command.service)
-
-	def writeLocalCmd( self, command, options ):
-		
-		if command.shell:
-			return "\n\t\tCmd { %s %s %s }" % ( command.shell, command.executable, options)
-		else:
-			return "\n\t\tCmd { %s %s }" % ( command.executable, options)		
-	
 	def writeCmds( self, task ):
 		#hand over the task these commands belong to as it may hold some information needed by each command
 		
@@ -138,7 +90,6 @@ class Serializer(  ):
 				self.tags( command )
 				
 		return output
-
 		
 	def spool( self, destination=None, startpaused=True, user=None ):
 		"""The destination can be any one of 'stdout'/'tractor'/'disk'. To launch the render but not have it
@@ -173,3 +124,99 @@ class Serializer(  ):
 				
 		if destination is 'stdout' or not destination :
 			print self.jobscript
+	
+class Serializer( SerializerBase ):
+	"""Tractor based serialization class. When spool() is called on a job object the 
+	tree of tasks is walked down, serializing all nodes in the tree into their alf script
+	equivalents.
+	"""
+
+	def __init__( self, obj ):
+		self.jobSerializableAttrs  = [ 'globalvars', 'tags', 'tasks' ]
+		self.taskSerializableAttrs = {}
+		self.cmdSerializableAttrs = {}
+
+		super(Serializer, self).__init__( obj )
+
+
+	######################################
+	#
+	# Serialization methods. 
+	#
+	######################################
+	
+	def atleast( self, node ):
+		"""returns the minimum number of procs for the node:\n\n\t "-atleast %%d" """
+		return " -atleast %d" % node.atleast
+	
+	def atmost( self, node ):
+		"""eturns the maximum number of procs for the node:\n\n\t  "-atmost %%d" """
+		return " -atmost %d" % node.atmost
+	
+	def chaser( self, node ):
+		"""returns the command to display ouput once a task has completed:\n\n\t "-chaser {%%s} """
+		return " -chaser {%s}" % node.chaser	
+	
+	def commands( self, node ):
+		"""returns a string containing commands for  a task:\n\n\t "-cmds { command/s }" """
+		return " -cmds { %s \n\t}" % self.writeCmds( node )
+	
+	def init( self, node ):
+		"""Sets global job variables using the Assign keyworkd:\n\n\t "-init{ Assign A {B} }" """
+		output + " -init { "
+		for var in node.globalvars:
+			output += "\nAssign %s {%s}" % ( var, node.globalvars[var] )
+		output += "\n}"
+	
+	def jobtitle( self, node ):
+		"""returns a string containing the  start of an alf script:\n\n\t "Job -title {example job}" """
+		return "\nJob -title { %s } " % node.title
+	
+	def preview( self, node ):
+		"""returns the command to display output during processing:\n\n\t "-preview {%%s} """
+		return " -preview {%s}" % node.preview
+	
+	def serialsubtasks( self, node ):
+		"""returns a flag for if the children of this node should be executed in serial:\n\n\t "-serialsubtasks 1" """
+		return " -serialsubtasks 1"
+		
+	def service( self, node ):
+		"""returns the list of services required for the task/job:\n\n\t "-service { servicename,servicename }" """
+		return " -service {%s}" % node.service	
+	
+	def subtasks( self, node ):
+		"""returns a string containing all of the subtasks for the current node:\n\n\t "-subtasks{ (N) }" """
+		output = " -subtasks {\n"
+		
+		for each in node.tasks:
+			output += self.writeNode( node.tasks[each] )
+		output += "\n}"
+	
+		return output
+	
+	def tags( self, node ):
+		"""returns tags for a task or job node:\n\n\t "-tags { tag/s }" """
+		return " -tags {%s}" % ",".join( node.tags )
+
+	def tasktitle( self, node ):
+		"""returns a string containing the  definition of a task:\n\n\t "Task {name } -id {1234}" """
+		return "\nTask { %s } -id { %s } " % ( node.label, node.name  )
+	
+	def writeLocalCmd( self, command, options ):
+		"""writes out a local command:\n\n\t "Cmd { executable options[] }" """
+		
+		if command.shell:
+			return "\n\t\tCmd { %s %s %s }" % ( command.shell, command.executable, options)
+		else:
+			return "\n\t\tCmd { %s %s }" % ( command.executable, options)		
+		
+	def writeRemoteCmd( self, command, options ):
+		"""writes out a remote command:\n\n\t "RemoteCmd { executable options[] }" """		
+		
+		if command.shell:
+			return "\n\t\tRemoteCmd { %s {%s %s} } -service {%s}" % ( command.shell, command.executable, options, command.service)
+		else:
+			return "\n\t\tRemoteCmd { %s %s } -service {%s}" % ( command.executable, options, command.service)
+
+	
+	
